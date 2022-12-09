f029a96432be893f0c0965719d6942fbff11d4cd
108-Software <75811746+108-Software@users.noreply.github.com>
2022-12-09 11:54:23 +0300

    Add files via upload

---
+++ Dmass.java
@@ -0,0 +1,80 @@
+public class Dmass<T> {
+
+    int Staticsize;
+    int counter = 0;
+    int place = 0;
+    int LastSize= (2*Staticsize)+10;
+    Object[] mass = new Object[LastSize];
+
+    Dmass(){}
+
+    public void AddFirst(T addf){
+        counter++;
+        if (place == mass.length-1){
+            LastSize=LastSize*2;
+            NewMass();
+        }
+        this.mass[place++]=addf;
+    }
+
+    public void AddLast(T addl){
+        counter++;
+        if (place == mass.length-1){
+            LastSize=LastSize*2;
+            NewMass();
+        }
+        mass[place++]=addl;
+    }
+
+    public void RemLast(){
+        mass[counter-1]="null";
+        counter--;
+    }
+
+    public void RemIndex(int REind) {
+        mass[REind] = "null";
+
+        try {if (REind>LastSize | REind<0 ) {
+            throw new java.lang.ArrayIndexOutOfBoundsException();}
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.print("Такой ячейки не существует");
+        }
+    }
+    public void AddIndex(int ADDind, T data){
+        mass[ADDind--]= data;
+        try {if (ADDind>LastSize | ADDind<0 ) {
+            throw new java.lang.ArrayIndexOutOfBoundsException();}
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.print("Такой ячейки не существует");
+        }
+    }
+
+    public void RemAll(){
+        for (int i=0; i<LastSize; i++){
+            mass[i]="null";
+        }
+        counter=0;
+    }
+
+    public void NewMass(){
+        Object[] Mmass = new Object[2*LastSize];
+        System.arraycopy(mass,0,Mmass,0,counter);
+        mass=Mmass;
+    }
+
+    public void Size(){
+        System.out.print(counter + "\n");
+    }
+
+    public void Print() {
+        if (counter <= LastSize) {
+            for (int i = 0; i < LastSize; i++) {
+                System.out.print("{" + mass[i] + "} ");
+            }
+            System.out.print("\n");
+
+        }
+    }
+}
---
+++ TestMass.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class TestMass <T>{
+    public static void main(String[] arg) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        Dmass Dm = new Dmass<>();
+
+        System.out.print("Введите размер массива: ");
+        int Staticsize = sc.nextInt();
+        Dm.Staticsize=Staticsize;
+        System.out.print(" 1. Добавление элемента в начало массива \n");
+        System.out.print(" 2. Добавление элемента в конец массива \n 3. Удаление последнего элемента массива \n");
+        System.out.print(" 4. Удаление элемента по индексу \n 5. Вставка элемента по ндексу \n");
+        System.out.print(" 6. Очистка массива \n 7. Узнать текущую длину \n");
+        System.out.print(" 8. Вывести массив \n 9. Завершить программу \n");
+
+        int answer = 0;
+        while (answer > 1 | answer < 9) {
+
+            answer = sc.nextInt();
+            var gh = sc.nextLine();
+
+            if (answer==9 | answer>9 | answer == 0 | answer<0){break;}
+
+            else{
+
+                switch (answer){
+
+                    case 1:
+                        System.out.print("Введите элемент которой хотите добавить: ");
+                        var addf = sc.nextLine();
+                        Dm.AddFirst(addf);
+                        break;
+
+                    case 2:
+                        System.out.print("Введите элемент которой хотите добавить: ");
+                        var addl = sc.nextLine();
+                        Dm.AddLast(addl);
+                        break;
+
+                    case 3:
+                        Dm.RemLast();
+                        break;
+
+                    case 4:
+                        System.out.print("Какой элемент хотите удалить? ");
+                        int REind = sc.nextInt();
+                        Dm.RemIndex(REind-1);
+                        break;
+
+                    case 5:
+                        System.out.print("На какое место ставим элемент? ");
+                        int ADDind = sc.nextInt();
+                        String Alo = sc.nextLine();
+                        System.out.print("Введите элемент: ");
+                        var data = sc.nextLine();
+                        Dm.AddIndex(ADDind, data);
+                        break;
+
+                    case 6:
+                        Dm.RemAll();
+                        break;
+
+                    case 7:
+                        Dm.Size();
+                        break;
+
+                    case 8:
+                        Dm.Print();
+                        break;
+
+                }
+            }
+        }
+    }
+}
